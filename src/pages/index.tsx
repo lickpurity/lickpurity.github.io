52db70616feefe93df7ec90df37916090319e0d7
lickpurity <130180988+lickpurity@users.noreply.github.com>
2023-04-07 14:05:21 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,10 +11,7 @@ export default function Home() {
         <div className="max-w-3xl text-center">
           <h1 className="font-bold text-3xl">Lick Purity Test</h1>
           <p className="my-4 italic">
-            The Purity Test has unhistorically served as a segue from 0-week to
-            true high school life at Lick-Wilmerding High School. It&apos;s a
-            voluntary opportunity for students to track the maturation of their
-            experiences throughout high school.
+            This purity test is an opportunity for Lick-Wilmerding students to track the maturation of their experiences throughout high school.
           </p>
         </div>
         <div className="max-w-3xl">
