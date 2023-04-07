a7e053de3c1ca5479e4a7810d8fba31e2362207b
lickpurity <130180988+lickpurity@users.noreply.github.com>
2023-04-07 12:46:11 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: '/lickpurity.github.io/',
   images: {
     unoptimized: true,
   },
